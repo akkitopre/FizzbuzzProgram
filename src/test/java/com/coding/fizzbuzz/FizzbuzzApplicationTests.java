63c5c6afd338a92743f748895ec074c6e4a8d030
akkitopre <akkitopre@gmail.com>
2026-01-22 22:12:14 +0530

    Test: Optimizing junit code

--- src/test/java/com/coding/fizzbuzz/FizzbuzzApplicationTests.java
+++ src/test/java/com/coding/fizzbuzz/FizzbuzzApplicationTests.java
@@ -12,24 +12,28 @@ class FizzbuzzApplicationTests {
 
 	FizzBuzzContext context = new FizzBuzzContext();
 
+	// should return Fizz since it is divisible by 3
 	@Test
 	void testFizz() {
 		assertEquals("Fizz", context.fizzBuzz(3));
 		assertEquals("Fizz", context.fizzBuzz(6));
 	}
-
+	
+	// should return Buzz since it is divisible by 5
 	@Test
 	void testBuzz() {
 		assertEquals("Buzz", context.fizzBuzz(5));
 		assertEquals("Buzz", context.fizzBuzz(10));
 	}
 
+	// should return FizzBuzz since it is divisible by both 3 and 5
 	@Test
 	void testFizzBuzz() {
 		assertEquals("FizzBuzz", context.fizzBuzz(15));
 		assertEquals("FizzBuzz", context.fizzBuzz(30));
 	}
 
+	// should return same number since it is not divisible by 3 or 5
 	@Test
 	void testDefault() {
 		assertEquals("1", context.fizzBuzz(1));
