fd0808b19e1bef4dccd5a67582e3ef53f709034f
akkitopre <akkitopre@gmail.com>
2026-01-22 20:04:41 +0530

    Feat: Divisible by 3 code logic

--- src/main/java/com/coding/fizzbuzz/FizzbuzzApplication.java
+++ src/main/java/com/coding/fizzbuzz/FizzbuzzApplication.java
@@ -1,13 +1,30 @@
 package com.coding.fizzbuzz;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FizzbuzzApplication {
+	
+	static boolean calculatedValue = false;
 
 	public static void main(String[] args) {
-		SpringApplication.run(FizzbuzzApplication.class, args);
+
+		for(int i = 1; i <= 50; i++)
+		{
+			calculatedValue = calculateDivisibleBy3(i);
+			if(calculatedValue)
+			{
+				System.out.println(i+" -> "+"Fizz");
+			}
+			else
+				System.out.println(i+" -> "+calculatedValue);
+		}
+		
+	}
+	
+	public static boolean calculateDivisibleBy3(int number)
+	{
+		return number % 3 == 0;
 	}
 
 }
--- src/test/java/com/coding/fizzbuzz/FizzbuzzApplicationTests.java
+++ src/test/java/com/coding/fizzbuzz/FizzbuzzApplicationTests.java
@@ -1,13 +1,31 @@
 package com.coding.fizzbuzz;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FizzbuzzApplicationTests {
-
+	
+	// should return true in case of number 3
 	@Test
-	void contextLoads() {
+	void testDivisibleBy3_with3_shouldReturnTrue() {
+		
+		int number = 3;
+		boolean calculatedValue = FizzbuzzApplication.calculateDivisibleBy3(number);
+		assertEquals(true, calculatedValue);
+		
+	}
+	
+	// should return false in case of number 5
+	@Test
+	void testDivisibleBy3_with5_shouldReturnFalse() {
+		
+		int number = 5;
+		boolean calculatedValue = FizzbuzzApplication.calculateDivisibleBy3(number);
+		assertEquals(false, calculatedValue);
+		
 	}
 
 }
